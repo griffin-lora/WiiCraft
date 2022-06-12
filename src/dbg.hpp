4f2b6f6400bf2e970f8ce6e9be2f5ecd7d27cb4b
TrafficConeGod <TrafficConeGod@github.com>
2022-06-12 19:11:41 -0400

    add dbg::non_copyable

--- src/dbg.hpp
+++ src/dbg.hpp
@@ -22,4 +22,12 @@ namespace dbg {
             freeze();
         }
     }
+
+    struct non_copyable {
+        non_copyable() = default;
+        non_copyable(const non_copyable&) = delete;
+        non_copyable& operator=(const non_copyable&) = delete;
+        non_copyable(non_copyable&&) {}
+        non_copyable& operator=(non_copyable&&) { return *this; }
+    };
 };
