77b1ca055448cb9c8a2cb026e69a2508b35eb5c9
TrafficConeGod <TrafficConeGod@github.com>
2022-06-14 20:04:46 -0400

    fix weird tab

--- src/game/util.hpp
+++ src/game/util.hpp
@@ -5,8 +5,8 @@ namespace game {
     template<typename T, typename F>
     constexpr void iterate_positions_in_sphere(T radius, F func) {
         for (T x = -radius; x <= radius; x++) {
-			for (T y = -radius; y <= radius; y++) {
-				for (T z = -radius; z <= radius; z++) {
+            for (T y = -radius; y <= radius; y++) {
+                for (T z = -radius; z <= radius; z++) {
                     glm::vec<3, T, glm::defaultp> pos = {x, y, z};
                     if (math::squared_length(pos) <= (radius * radius)) {
                         func(pos);
