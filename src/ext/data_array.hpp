f38fd36bfe8175b72aae747b442f98a176d584cd
TrafficConeGod <TrafficConeGod@github.com>
2022-06-16 21:16:29 -0400

    add slight optimization to data_array

--- src/ext/data_array.hpp
+++ src/ext/data_array.hpp
@@ -9,14 +9,12 @@ namespace ext {
         static_assert(std::is_pod_v<T>, "T must be a POD type.");
         T* m_data = nullptr;
         std::size_t m_size = 0;
-        std::size_t m_data_size = 0;
         public:
             inline data_array() = default;
-            inline data_array(std::size_t size) : m_data((T*)malloc(size * sizeof(T))), m_size(size), m_data_size(size) {}
-            inline data_array(data_array&& other) : m_data(other.m_data), m_size(other.m_size), m_data_size(other.m_data_size) {
+            inline data_array(std::size_t size) : m_data((T*)malloc(size * sizeof(T))), m_size(size) {}
+            inline data_array(data_array&& other) : m_data(other.m_data), m_size(other.m_size) {
                 other.m_data = nullptr;
                 other.m_size = 0;
-                other.m_data_size = 0;
             }
 
             inline ~data_array() {
@@ -34,9 +32,7 @@ namespace ext {
                 }
                 m_data = other.m_data;
                 m_size = other.m_size;
-                m_data_size = other.m_data_size;
                 other.m_size = 0;
-                other.m_data_size = 0;
                 other.m_data = nullptr;
                 return *this;
             }
@@ -48,24 +44,14 @@ namespace ext {
             using const_iterator = __gnu_cxx::__normal_iterator<const T*, data_array>;
 
             inline void resize_without_copying(std::size_t new_size) {
+                if (new_size == m_size) {
+                    return;
+                }
                 if (m_data != nullptr) {
                     free(m_data);
                 }
                 m_data = (T*)malloc(new_size * sizeof(T));
                 m_size = new_size;
-                m_data_size = new_size;
-            }
-
-            inline void resize_without_copying_aligned_if_greater_size(std::size_t alignment, std::size_t new_size) {
-                if (new_size > m_data_size) {
-                    if (m_data != nullptr) {
-                        free(m_data);
-                    }
-                    m_data = (T*)memalign(alignment, new_size * sizeof(T));
-                    m_data_size = new_size;
-                }
-                m_size = new_size;
-                
             }
 
             inline T* data() {
