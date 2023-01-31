4d5b596268db288c7496cb7f7378b606e3e7b282
TrafficConeGod <trafficconegod@github.com>
2023-01-30 21:27:10 -0500

    add the remaining parts of memory

--- src/game/mem.h
+++ src/game/mem.h
@@ -26,15 +26,28 @@ typedef block_type_t block_chunk_t[16 * 16 * 16];
 _Static_assert(sizeof(block_chunk_t) == 4096, "");
 
 typedef struct {
-    // Building quads in upper half of cache
+    display_list_chunk_t secondary_display_list_chunks[3]; // These take up the same location in cache as the building quads and thus are the secondary choice in the event of running out of display list chunks
+    display_list_chunk_t primary_display_list_chunks[4];
+    block_chunk_t block_chunk;
+} game_cache_chunk_t;
+
+_Static_assert(sizeof(game_cache_chunk_t) == 32768, "");
+
+typedef struct {
+    // First cache chunk
+    // Building quads (hopefully) locked in upper half of cache
     struct {
         _Alignas(32) block_quad_t solid[NUM_SOLID_BUILDING_QUADS];
         _Alignas(32) block_quad_t transparent[NUM_TRANSPARENT_BUILDING_QUADS];
         _Alignas(32) block_quad_t transparent_double_sided[NUM_TRANSPARENT_DOUBLE_SIDED_BUILDING_QUADS];
     } building_quads;
-    // display_list_chunk_t display_list_chunks_a[4];
-    // block_chunk_t block_chunk_a;
+    _Alignas(4096) display_list_chunk_t display_list_chunks[4];
+    _Alignas(4096) block_chunk_t block_chunk;
+    // End of first cache chunk
+    // The remaining cache chunks
+    _Alignas(32768) game_cache_chunk_t cache_chunks[639];
 } game_memory_t;
+_Static_assert(sizeof(game_memory_t) == 0x1400000, "");
 
 extern game_memory_t gmem;
 _Static_assert(sizeof(gmem.building_quads) == 4096*3, "");
