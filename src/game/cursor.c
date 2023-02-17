63eaf8094f695156f30403677da4ccc8b96795ac
TrafficConeGod <trafficconegod@github.com>
2023-02-17 13:01:46 -0500

    use static disp lists for cursor

--- src/game/cursor.c
+++ src/game/cursor.c
@@ -8,36 +8,20 @@
 
 #define VERTEX_COUNT 4
 
-#define DISP_LIST_SIZE (ALIGN_TO_32( \
+#define DISP_LIST_SIZE (ALIGN_TO_32_NEW( \
     GET_BEGIN_INSTRUCTION_SIZE(VERTEX_COUNT) + \
     GET_VECTOR_INSTRUCTION_SIZE(2, sizeof(u8), VERTEX_COUNT) + \
     GET_VECTOR_INSTRUCTION_SIZE(2, sizeof(u8), VERTEX_COUNT) \
 ))
-_Alignas(32) static u8 disp_list[DISP_LIST_SIZE];
-static size_t disp_list_size;
+_Alignas(32) static u8 disp_list[DISP_LIST_SIZE] = {
+    0x83, 0x0, 0x4, 0x0, 0x0, 0x0, 0x0, 0x30, 0x0, 0x1, 0x0, 0x30, 0x30, 0x1, 0x1, 0x0, 0x30, 0x0, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0
+};
 
 static Mtx model_view;
 
 void cursor_init(void) {
 	GX_SetVtxAttrFmt(VERTEX_FORMAT_INDEX, GX_VA_POS, GX_POS_XY, GX_U8, 0);
 	GX_SetVtxAttrFmt(VERTEX_FORMAT_INDEX, GX_VA_TEX0, GX_TEX_ST, GX_U8, 4);
-
-    GX_BeginDispList(disp_list, DISP_LIST_SIZE);
-
-    GX_Begin(GX_QUADS, VERTEX_FORMAT_INDEX, VERTEX_COUNT);
-
-    GX_Position2u8(0, 0);
-    GX_TexCoord2u8(0, 0);
-    GX_Position2u8(48, 0);
-    GX_TexCoord2u8(1, 0);
-    GX_Position2u8(48, 48);
-    GX_TexCoord2u8(1, 1);
-    GX_Position2u8(0, 48);
-    GX_TexCoord2u8(0, 1);
-
-    GX_End();
-
-    disp_list_size = GX_EndDispList();
 }
 
 void cursor_update(u16 v_width, u16 v_height) {
@@ -50,5 +34,5 @@ void cursor_draw(void) {
 	GX_SetTevOrder(GX_TEVSTAGE0, GX_TEXCOORD0, GX_TEXMAP1, GX_COLOR0A0);
     
 	GX_SetCurrentMtx(MATRIX_INDEX);
-    GX_CallDispList(disp_list, disp_list_size);
+    GX_CallDispList(disp_list, DISP_LIST_SIZE);
 }
